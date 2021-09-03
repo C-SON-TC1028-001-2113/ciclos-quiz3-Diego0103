579fcaaf1da3e4c4ed301d7de41520a06eab7a54
Diego0103 <A01254073@itesm.mx>
2021-09-03 16:33:44 +0000

    05PromedioSencillo

--- assignments/05PromedioSencillo/src/exercise.py
+++ assignments/05PromedioSencillo/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+    n=int(input())
+    c=1
+    suma=0
+    numero=0
+    while c<=n:
+        numero=float(input())
+        suma+=numero
+        promedio=suma/n
+        c+=1
+    print(promedio)
 
+        
 if __name__=='__main__':
     main()
